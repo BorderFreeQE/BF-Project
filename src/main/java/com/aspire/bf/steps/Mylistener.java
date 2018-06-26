5d3ba98246921de2d92885389b9080b6dd3564e2
amer <aziyadeh@aspire-infotech.net>
2018-06-26 12:33:35 +0300

    full run edit on master

--- src/main/java/com/aspire/bf/steps/Mylistener.java
+++ src/main/java/com/aspire/bf/steps/Mylistener.java
@@ -25,147 +25,147 @@ public class Mylistener implements TestEventListener {
     	@Override
         public void beforeStep(String storyName, String scenarioName, Map<String, String> exampleData,
                 String stepName) {
-            
-    		
-    		AspireWebElement lastelement = aspireBrowserState.getLastAccessedElement();
-    		
-            	try
-            	{
-            		if (AspireBrowser.getElementByPropertyNameGlobaly("loveRuelal").isDisplayed())
-                    {
-             		 AspireBrowser.getElementByPropertyNameGlobaly("loveRuelalaClose").click();
-             		 
-             		System.out.println("Ruelala Dialog closed");
-                    }
-             	  
-            	}
-            	catch (NoSuchElementException e)
-        		{
-        			
-        			try
-        			{
-                		if(AspireBrowser.getElementByPropertyNameGlobaly("giltSurveyDialog").isDisplayed()) 
-                 	    {
-                 		 AspireBrowser.getElementByPropertyNameGlobaly("giltCloseSurveyDialog").click();
-                 
-                 		System.out.println("Gilt Dialog closed");
-                 	     }
-        			}
-        			catch (NoSuchElementException e1)
-        			{
-        				
-        				try
-        				{
-        					if (AspireBrowser.getElementByPropertyNameGlobaly("barneysFeedbackDialog").isDisplayed())
-                     	    {
-                     		 AspireBrowser.getElementByPropertyNameGlobaly("barneysFeedbackClose").click();
-                     		 
-                     		System.out.println("Barneys Dialog closed");
-                     	     }
-        				}
-        				catch (NoSuchElementException e2)
-        				{
-        					
-        					try
-        					{
-        						if (AspireBrowser.getElementByPropertyNameGlobaly("underarmourFeedBackDialog").isDisplayed())
-                         	    {
-                         		 AspireBrowser.getElementByPropertyNameGlobaly("underarmourFeedBackDialogCloseButton").click();
-                         		 
-                         		System.out.println("Under Armour Dialog closed");
-                         	     }
-        					}
-        					catch (NoSuchElementException e3)
-        					{
-        						
-        						try
-            					{
-            						if (AspireBrowser.getElementByPropertyNameGlobaly("bergdorfSimilarDialog").isDisplayed())
-                             	    {
-                             		 AspireBrowser.getElementByPropertyNameGlobaly("bergdorfSimilarDialogNoThanks").scrollToElement().click();
-                             		 
-                             		System.out.println("Bergdorf Dialog closed");
-                             	     }
-            					}
-            					catch (NoSuchElementException e4)
-            					{
-            						
-            						try
-                					{
-                						if (AspireBrowser.getElementByPropertyNameGlobaly("llbeanShipToDialog").isDisplayed())
-                                 	    {
-                                 		 AspireBrowser.getElementByPropertyNameGlobaly("llbeanShipToDialogClose").scrollToElement().click();
-                                 	
-                                 		System.out.println("L.L.Bean Dialog closed");
-                                 	     }
-                					}
-                					catch (NoSuchElementException e5)
-                					{
-                						try
-                    					{
-                    						if (AspireBrowser.getElementByPropertyNameGlobaly("hisroomFeed").isDisplayed())
-                                     	    {
-                                     		 AspireBrowser.getElementByPropertyNameGlobaly("hisroomFeedClose").scrollToElement().click();
-                                     		
-                                     		 Thread.sleep(5000);
-                                     		 System.out.println("His Room Dialog closed");
-                                     	     }
-                    					}
-                    					catch (NoSuchElementException | InterruptedException e6)
-                    					{
-                    						try
-                        					{
-                        						if (AspireBrowser.getElementByPropertyNameGlobaly("herroomFeed").isDisplayed())
-                                         	    {
-                                         		 AspireBrowser.getElementByPropertyNameGlobaly("herroomFeedClose").scrollToElement().click();
-                                         		
-                                         		 Thread.sleep(5000);
-                                         		 System.out.println("Her Room Dialog closed");
-                                         	     }
-                        					}
-                        					catch (NoSuchElementException | InterruptedException e7)
-                        					{
-                        						/*try
-                            					{
-                            						if (AspireBrowser.getElementByPropertyNameGlobaly("bcbgEmailDialog").isDisplayed())
-                                             	    {
-                                             		 AspireBrowser.getElementByPropertyNameGlobaly("bcbgEmailDialogClose").click();
-                                             		
-                                             		 Thread.sleep(5000);
-                                             		 System.out.println("BCBG Dialog closed");
-                                             	     }
-                            					}*/
-                            					/*catch (NoSuchElementException | InterruptedException e8)
-                            					{*/
-                            						try
-                                					{
-                                						if (AspireBrowser.getElementByPropertyNameGlobaly("gapFeedBackDialog").isDisplayed())
-                                                 	    {
-                                                 		 AspireBrowser.getElementByPropertyNameGlobaly("gapFeedBackCloseDialog").click();
-                                                 		
-                                                 		 Thread.sleep(5000);
-                                                 		 System.out.println("Her Room Dialog closed");
-                                                 	     }
-                                					}
-                                					catch (NoSuchElementException | InterruptedException e9)
-                                					{
-                                						
-                                					}
-                            					/*}*/
-                        					}
-                    					}
-                					}
-            					}
-        					}
-        				}
-        			}
-        		}
-            
-           
-            
-            
-            
-            aspireBrowserState.setLastAccessedElement(lastelement);
+//            
+//    		
+//    		AspireWebElement lastelement = aspireBrowserState.getLastAccessedElement();
+//    		
+//            	try
+//            	{
+//            		if (AspireBrowser.getElementByPropertyNameGlobaly("loveRuelal").isDisplayed())
+//                    {
+//             		 AspireBrowser.getElementByPropertyNameGlobaly("loveRuelalaClose").click();
+//             		 
+//             		System.out.println("Ruelala Dialog closed");
+//                    }
+//             	  
+//            	}
+//            	catch (NoSuchElementException e)
+//        		{
+//        			
+//        			try
+//        			{
+//                		if(AspireBrowser.getElementByPropertyNameGlobaly("giltSurveyDialog").isDisplayed()) 
+//                 	    {
+//                 		 AspireBrowser.getElementByPropertyNameGlobaly("giltCloseSurveyDialog").click();
+//                 
+//                 		System.out.println("Gilt Dialog closed");
+//                 	     }
+//        			}
+//        			catch (NoSuchElementException e1)
+//        			{
+//        				
+//        				try
+//        				{
+//        					if (AspireBrowser.getElementByPropertyNameGlobaly("barneysFeedbackDialog").isDisplayed())
+//                     	    {
+//                     		 AspireBrowser.getElementByPropertyNameGlobaly("barneysFeedbackClose").click();
+//                     		 
+//                     		System.out.println("Barneys Dialog closed");
+//                     	     }
+//        				}
+//        				catch (NoSuchElementException e2)
+//        				{
+//        					
+//        					try
+//        					{
+//        						if (AspireBrowser.getElementByPropertyNameGlobaly("underarmourFeedBackDialog").isDisplayed())
+//                         	    {
+//                         		 AspireBrowser.getElementByPropertyNameGlobaly("underarmourFeedBackDialogCloseButton").click();
+//                         		 
+//                         		System.out.println("Under Armour Dialog closed");
+//                         	     }
+//        					}
+//        					catch (NoSuchElementException e3)
+//        					{
+//        						
+//        						try
+//            					{
+//            						if (AspireBrowser.getElementByPropertyNameGlobaly("bergdorfSimilarDialog").isDisplayed())
+//                             	    {
+//                             		 AspireBrowser.getElementByPropertyNameGlobaly("bergdorfSimilarDialogNoThanks").scrollToElement().click();
+//                             		 
+//                             		System.out.println("Bergdorf Dialog closed");
+//                             	     }
+//            					}
+//            					catch (NoSuchElementException e4)
+//            					{
+//            						
+//            						try
+//                					{
+//                						if (AspireBrowser.getElementByPropertyNameGlobaly("llbeanShipToDialog").isDisplayed())
+//                                 	    {
+//                                 		 AspireBrowser.getElementByPropertyNameGlobaly("llbeanShipToDialogClose").scrollToElement().click();
+//                                 	
+//                                 		System.out.println("L.L.Bean Dialog closed");
+//                                 	     }
+//                					}
+//                					catch (NoSuchElementException e5)
+//                					{
+//                						try
+//                    					{
+//                    						if (AspireBrowser.getElementByPropertyNameGlobaly("hisroomFeed").isDisplayed())
+//                                     	    {
+//                                     		 AspireBrowser.getElementByPropertyNameGlobaly("hisroomFeedClose").scrollToElement().click();
+//                                     		
+//                                     		 Thread.sleep(5000);
+//                                     		 System.out.println("His Room Dialog closed");
+//                                     	     }
+//                    					}
+//                    					catch (NoSuchElementException | InterruptedException e6)
+//                    					{
+//                    						try
+//                        					{
+//                        						if (AspireBrowser.getElementByPropertyNameGlobaly("herroomFeed").isDisplayed())
+//                                         	    {
+//                                         		 AspireBrowser.getElementByPropertyNameGlobaly("herroomFeedClose").scrollToElement().click();
+//                                         		
+//                                         		 Thread.sleep(5000);
+//                                         		 System.out.println("Her Room Dialog closed");
+//                                         	     }
+//                        					}
+//                        					catch (NoSuchElementException | InterruptedException e7)
+//                        					{
+//                        						/*try
+//                            					{
+//                            						if (AspireBrowser.getElementByPropertyNameGlobaly("bcbgEmailDialog").isDisplayed())
+//                                             	    {
+//                                             		 AspireBrowser.getElementByPropertyNameGlobaly("bcbgEmailDialogClose").click();
+//                                             		
+//                                             		 Thread.sleep(5000);
+//                                             		 System.out.println("BCBG Dialog closed");
+//                                             	     }
+//                            					}*/
+//                            					/*catch (NoSuchElementException | InterruptedException e8)
+//                            					{*/
+//                            						try
+//                                					{
+//                                						if (AspireBrowser.getElementByPropertyNameGlobaly("gapFeedBackDialog").isDisplayed())
+//                                                 	    {
+//                                                 		 AspireBrowser.getElementByPropertyNameGlobaly("gapFeedBackCloseDialog").click();
+//                                                 		
+//                                                 		 Thread.sleep(5000);
+//                                                 		 System.out.println("Her Room Dialog closed");
+//                                                 	     }
+//                                					}
+//                                					catch (NoSuchElementException | InterruptedException e9)
+//                                					{
+//                                						
+//                                					}
+//                            					/*}*/
+//                        					}
+//                    					}
+//                					}
+//            					}
+//        					}
+//        				}
+//        			}
+//        		}
+//            
+//           
+//            
+//            
+//            
+//            aspireBrowserState.setLastAccessedElement(lastelement);
         }
 }
 
