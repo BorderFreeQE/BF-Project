d6934725943a92f57f4ccda07f67f1f0a58ea280
amer <aziyadeh@aspire-infotech.net>
2018-07-01 10:13:19 +0300

    Add new branch to fixed test cases

--- src/main/java/com/aspire/bf/pages/GapPage.java
+++ src/main/java/com/aspire/bf/pages/GapPage.java
@@ -5,7 +5,7 @@ import com.aspire.automation.web.util.AspireWebElements;
 import com.aspire.automation.web.util.annotation.CssSelector;
 import com.aspire.automation.web.util.annotation.Page;
 
-@Page(name = "gap", url = "${pages.gap.gapUrl}")
+@Page(name = "gap", url = "${gapUrl}")
 public interface GapPage {
 
 	@CssSelector("${pages.gap.gapClosePromoIcon}")
